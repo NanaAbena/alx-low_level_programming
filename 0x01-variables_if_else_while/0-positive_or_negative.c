07ecf8178a85062c526883ddf367e147fadbf0e5
NanaAbena <nanykyeraa@gmail.com>
2023-04-21 16:54:37 -0700

    3 hours gone

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,14 +11,14 @@
   *
   */
 
- int main(void) 
+int main(void)
  { 
 
 
-         int n; 
-  
-         srand(time(0)); 
-         n = rand() - RAND_MAX / 2;
+	 int n;
+
+	 srand(time(0));
+ 	 n = rand() - RAND_MAX / 2;
 
 	 if (n > 0)
 	 {
@@ -32,6 +32,6 @@
 	 {
 	 printf("%d is negative\n", n);
 	 }
-         return (0);
+	 return (0);
 
  }
