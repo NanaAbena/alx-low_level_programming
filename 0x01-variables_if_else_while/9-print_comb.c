c4d9031c400dce06282985b14fb34c77141017cc
NanaAbena <nanykyeraa@gmail.com>
2023-04-22 14:58:25 -0700

    Lets make it now

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -23,29 +23,3 @@ int main(void)
 
 	return (0);
 }
-
-#include <stdio.h>
-
-int main(void)
-{
-	int b, c;
-
-	for (b = 0; b <= 9; b++)
-	{
-		for (b = c;  c <= 9; c++)
-		{
-			putchar(b + '0');
-			putchar(c + '0');
-			if (b != 9 ||  b != 9)
-			{
-				putchar(',');
-				putchar(',');
-			}
-		}
-	}
-
-
-	putchar('\n');
-
-	return (0);
-}
