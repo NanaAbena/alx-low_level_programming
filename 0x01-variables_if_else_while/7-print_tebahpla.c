ac6002f756a1e9df5b217f4aba15cc883387fa3a
NanaAbena <nanykyeraa@gmail.com>
2023-04-22 13:12:59 -0700

    Lets mit it up

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char l;
+
+	for (lr = 'z'; l >= 'a'; l--)
+	{
+		putchar(l);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
