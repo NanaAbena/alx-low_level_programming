8bbf0c9cf75f93ada51595aad8fa70ff75bce811
NanaAbena <nanykyeraa@gmail.com>
2023-04-22 15:27:28 -0700

    Advanced task 0

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int digit3, digit4;
+
+	for (digit3 = 0; digit3 < 9; digit3++)
+	{
+		for (digit4 = digit3 + 1; digit4 < 10; digit4++)
+		{
+			putchar((digit3 % 10) + '0');
+			putchar((digit4 % 10) + '0');
+			if (digit3 == 8 && digit4 == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
