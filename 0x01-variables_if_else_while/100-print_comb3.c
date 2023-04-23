18d42da95affe9d43416e212eafd52848ba8845a
NanaAbena <nanykyeraa@gmail.com>
2023-04-23 09:53:11 -0700

    Advanced task one still on it

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,22 +16,17 @@ int main(void)
 		{
 			if (i != j)
 			{
-				putchar((i / 10) + '0');
-				putchar((i % 10) + '0');
-				putchar(',');
-				putchar(' ');
-				putchar((j / 10) + '0');
-				putchar((j % 10) + '0');
+				putchar(i / 10 + '0');
+				putchar(i % 10 + '0');
+				putchar(j / 10 + '0');
+				putchar(j % 10 + '0');
 
 				if (i != 98 || j != 99)
-				{
-					putchar(',');
-					putchar(' ');
-				}
+					putchar(44);
 			}
 		}
 	}
 
-	putchar('\n');
+	putchar(10);
 	return (0);
 }
