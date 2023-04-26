0d63ba2a4a051a5110b43b87ba6f56b86b7922da
NanaAbena <nanykyeraa@gmail.com>
2023-04-26 14:43:26 -0700

    Almost done with mandatory

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - add two integers and returns the result
+ *
+ * @r: integer one
+ * @s: integer two
+ *
+ * Return: Add of integer one and integer two
+ */
+int add(int r, int s)
+{
+	return (r + s);
+}
