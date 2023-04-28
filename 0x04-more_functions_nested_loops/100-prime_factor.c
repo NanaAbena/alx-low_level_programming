3f59b2cb95809486c67cfbd62115eefc35861acb
NanaAbena <nanykyeraa@gmail.com>
2023-04-27 20:35:59 -0700

    Not going well

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the largest prime factor of 612852475143
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	unsigned long int i, n = 612852475143;
+
+		n = n / i;
+	printf("%lu\n", n);
+	return (0);
+}
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,22 +10,29 @@
  *
  * Return: always 0
  */
+
 int main(void)
 {
-	int i;
+    for (int i = 1; i <= 100; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+            printf("FizzBuzz ");
+        }
+        else if (i % 3 == 0)
+        {
+            printf("Fizz ");
+        }
+        else if (i % 5 == 0)
+        {
+            printf("Buzz ");
+        }
+        else
+        {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
 
-	for (i = 1; i <= 100; i++)
-	{
-		if (i % 15 == 0)
-			printf("FizzBuzz");
-		else if (1 % 3 == 0)
-			printf("Fizz");
-		else if
-			(i % 5 == 0);
-		printf("Buzz");
-		else
-		printf("%i", i);
-	}
-printf("\n");
-return (0);
+    return 0;
 }
