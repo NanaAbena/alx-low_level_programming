77c133447abc97835e5ecbe2b72c9d900b32002b
NanaAbena <nanykyeraa@gmail.com>
2023-05-02 07:08:24 -0700

    My first pointer made

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates an int parameter to the value of zero
+ * @n: character to check
+ * Return: 0 on a success.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
