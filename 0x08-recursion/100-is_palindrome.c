fa12670e08e5869fd92a47a1b9896505bfd54f59
NanaAbena <nanykyeraa@gmail.com>
2023-05-10 18:35:01 -0700

    Palindrone

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-int check_pal(char *s, int i, int len);
 int _strlen_recursion(char *s);
 
 /**
@@ -13,7 +12,7 @@ int is_palindrome(char *s)
 {
 	if (*s == 0)
 		return (1);
-	return (check_pal(s, 0, _strlen_recursion(s)));
+	return (0);
 }
 
 /**
