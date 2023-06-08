7aead022932143b9438910999c386de7b249bdfa
NanaAbena <nanykyeraa@gmail.com>
2023-06-08 15:21:31 -0700

    Converting and printing on binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary equivalent of a decimal number
+ * @n: number to print in binary
+ */
+void print_binary(unsigned long int n)
+{
+	int u, note = 0;
+	unsigned long int new;
+
+	for (u = 63; u >= 0; u--)
+
+		new = n >> u;
+
+	if (new & 1)
+	{
+		_putchar('1');
+		note++;
+	}
+	else if (note)
+		_putchar('0');
+	if (!note)
+	{
+	_putchar('0');
+	}
+}
